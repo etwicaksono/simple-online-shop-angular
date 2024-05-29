c84344dcfcb8ff9038ae8aef8d89d1183d49fd68
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-05-29 14:51:20 +0700

    Set header as standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerListComponent } from './feature/customer/customer-list/customer-list.component';
-import { HeaderComponent } from './core/components/header/header.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { SidebarModule } from 'primeng/sidebar';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HeaderComponent } from './core/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerListComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    SidebarModule,
+    HeaderComponent
   ],
   providers: [
     provideAnimationsAsync()
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    SidebarModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
 })
 export class HeaderComponent {
   sidebarVisible: boolean = false
