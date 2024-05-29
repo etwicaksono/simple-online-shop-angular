dee36810a10e03e6a025a0269abb0f2ac0e602a8
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-05-29 12:51:38 +0700

    Set header as responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomerListComponent } from './feature/customer/customer-list/customer-list.component';
 
 const routes: Routes = [
-  {
-    path: '', component:CustomerListComponent
-  }
+  { path: '', component: CustomerListComponent },
+  { path: 'customer-list', component: CustomerListComponent },
+  { path: 'item-list', component: CustomerListComponent },
+  { path: 'order-list', component: CustomerListComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { HeaderComponent } from './core/components/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { SidebarModule } from 'primeng/sidebar';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -20,9 +22,12 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    SidebarModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
