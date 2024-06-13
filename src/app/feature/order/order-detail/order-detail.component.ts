08b2bd87c5c3eba22e91811e8fc717c27020d0f0
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-06-13 23:24:09 +0700

    Feature: Order: Detail: Successfully create 'Detail Order' page

--- src/app/feature/order/order-detail/order-detail.component.ts
+++ src/app/feature/order/order-detail/order-detail.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormHelper } from '@app/core/helpers/form-helper';
+import { OrderService } from '@app/core/services/order.service';
 
 @Component({
   selector: 'app-order-detail',
   templateUrl: './order-detail.component.html',
   styleUrl: './order-detail.component.css'
 })
-export class OrderDetailComponent {
+export class OrderDetailComponent implements OnInit {
+  constructor(
+    private orderService: OrderService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {
+    this.orderId = Number(this.route.snapshot.paramMap.get('orderID'));
+  }
+
+  existingOrder: any = null;
+  orderId: number
+
+  ngOnInit(): void {
+    this.loadDetailOrder()
+  }
+
+  loadDetailOrder() {
+    this.orderService.findOrder(this.orderId).subscribe({
+      next: (res: any) => {
+        this.existingOrder = res.data
+      },
+      error: (res: HttpErrorResponse) => {
+        console.log("error: ", res);
+      }
+    })
+  }
+
+
 
 }
